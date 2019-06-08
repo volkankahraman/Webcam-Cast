92faad3118a3f121d3a1a1db5b67a60230063b5c
volkankahraman <khrmnvolkan@gmail.com>
2019-06-08 18:05:14 +0300

    initialize

---
+++ app.js
@@ -0,0 +1,50 @@
+const cv = require('opencv4nodejs');
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
+
+function lengthInUtf8Bytes(str) {
+    var m = encodeURIComponent(str).match(/%[89ABab]/g);
+    return str.length + (m ? m.length : 0);
+}
+
+const wCap = new cv.VideoCapture(0);
+const FPS = 15;
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/face-detect', function (req, res) {
+  res.sendFile(path.join(__dirname + '/face-detect.html'));
+});
+app.get('/image', function (req, res) {
+      
+      
+
+    var frame = wCap.read();
+
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    var img = new Buffer.from(image, 'base64');
+    res.writeHead(200, {
+        'Content-Type': 'image/jpeg',
+        'Content-Length': img.length
+      });
+      res.end(img);
+});
+var secoundDataCounter = 0,
+    secoundData = 0;
+setInterval(() => {
+    var frame = wCap.read();
+    secoundDataCounter++;
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    if (secoundDataCounter >= FPS) {
+        console.log((secoundData / 1000).toFixed(2) + " mb/s");
+        secoundDataCounter = 0;
+        secoundData = 0;
+    } else {
+        secoundData += lengthInUtf8Bytes(image) / 1000
+    }
+    io.emit('image', image);
+}, 1000 / FPS);
